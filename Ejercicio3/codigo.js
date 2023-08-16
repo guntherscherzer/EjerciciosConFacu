d70a093312c5106e1b6262783f77652e164eb703
guntherscherzer <guntherscherzer97@gmail.com>
2023-08-16 17:40:38 -0300

    Ejercicio2

---
+++ Ejercicio3/codigo.js
@@ -0,0 +1,11 @@
+//Modificar el siguiente script para que:
+//Todo el código JavaScript se encuentre en un archivo externo llamado codigo.js y el script siga funcionando de la misma manera.
+//Después del primer mensaje, se debe mostrar otro mensaje que diga "Soy el primer script"
+//Añadir algunos comentarios que expliquen el funcionamiento del código
+//Añadir en la página XHTML un mensaje de aviso para los navegadores que no tengan activado el soporte de JavaScript
+
+
+//al abrise el index, el codigo envia primero un msj de "Hola mundo" y seguido a eso un mensaje de "soy el primer script"
+
+console.log("Hola Mundo!");
+console.log("soy el primer script");
