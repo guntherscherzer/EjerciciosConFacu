fafa210bf9bf8390991899c089c573e44be473af
guntherscherzer <guntherscherzer97@gmail.com>
2023-08-16 17:37:03 -0300

    Ejercicio2

---
+++ Ejercicio2/codigo.js
@@ -0,0 +1,10 @@
+//El mensaje que se muestra al usuario se almacene en una variable llamada mensaje y el funcionamiento del script sea el mismo.
+//El mensaje mostrado sea el de la siguiente imagen:
+
+
+
+//al abrise el index, el codigo envia primero un msj de "Hola mundo" y seguido a eso un mensaje de "soy el primer script"
+
+let mensaje ="Hola mundo"+ "\nQue facil es incuir 'comillas simples'"+"\ny \"comillas dobles\"";
+
+console.log(mensaje);
