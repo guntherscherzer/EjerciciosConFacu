690c0b7f44953a106d40b586c00b848fc09964de
guntherscherzer <guntherscherzer97@gmail.com>
2023-08-16 17:54:37 -0300

    Ejercicio3

--- Ejercicio3/codigo.js
+++ Ejercicio3/codigo.js
@@ -1,11 +1,4 @@
-//Modificar el siguiente script para que:
-//Todo el código JavaScript se encuentre en un archivo externo llamado codigo.js y el script siga funcionando de la misma manera.
-//Después del primer mensaje, se debe mostrar otro mensaje que diga "Soy el primer script"
-//Añadir algunos comentarios que expliquen el funcionamiento del código
-//Añadir en la página XHTML un mensaje de aviso para los navegadores que no tengan activado el soporte de JavaScript
-
-
-//al abrise el index, el codigo envia primero un msj de "Hola mundo" y seguido a eso un mensaje de "soy el primer script"
-
-console.log("Hola Mundo!");
-console.log("soy el primer script");
+//Crear un array llamado meses y que almacene el nombre de los doce meses del año. 
+//Mostrar por pantalla los doce nombres utilizando la función console.log().
+let meses = ["Enero","Febrero","Marzo","Abril","Mayonesa","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+console.log(meses);
