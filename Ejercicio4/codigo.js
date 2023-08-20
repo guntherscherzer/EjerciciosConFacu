961810b1eacba6ade28f04240deb09fa8c835571
guntherscherzer <guntherscherzer97@gmail.com>
2023-08-20 15:42:53 -0300

    ejercicio 6

--- Ejercicio4/codigo.js
+++ Ejercicio4/codigo.js
@@ -3,4 +3,5 @@
 //Utilizando exclusivamente los dos valores booleanos del array, determinar los operadores necesarios para obtener un resultado true y otro resultado false
 //Determinar el resultado de las cinco operaciones matemáticas realizadas con los dos elementos numéricos
 
-Let valores = [true, 5, false, "hola", "adios", 2];
+let valores = [true, 5, false, "hola", "adios", 2];
+
