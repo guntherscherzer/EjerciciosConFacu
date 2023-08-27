6a67825d4d518f977d2c2eb92734161f6a09cc54
guntherscherzer <guntherscherzer97@gmail.com>
2023-08-27 18:12:32 -0300

    Ejercitacion 8 + correcciones

--- Ejercicio8/codigo.js
+++ Ejercicio8/codigo.js
@@ -1,7 +1,7 @@
 //Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve 
 //como resultado una cadena de texto que indica si el número es par o impar.
 //Mostrar por pantalla el resultado devuelto por la función.
-let numero = Number(prompt("Ingrese un numero"))
-    if () {
-        
-    }
+let numero = Number(prompt("Ingrese un numero"));
+    if ((numero % 2) == 0) {
+        alert("su numero es par");
+    }else alert("Su numero es impar");
