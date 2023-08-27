95233ae25f09e54201cbd8092b037b5777a730bd
guntherscherzer <guntherscherzer97@gmail.com>
2023-08-27 19:04:22 -0300

    correccion ejercicio 7

--- Ejercicio7/codigo.js
+++ Ejercicio7/codigo.js
@@ -2,8 +2,11 @@
 //factores n x (n-1) x (n-2) x ... x 1. Así, el factorial de 5 (escrito como 5!) es igual a: 5! = 5 x 4 x 3 x 2 x 1 = 120
 //Utilizando la estructura for, crear un script que calcule el factorial de un número entero.
 let numero = Number(prompt("igrese el numero que quiere factorizar"))
-let resul = 1
-for (let i = numero; i > 0; i--){
-    resul *= i;       
+let resul = numero
+for (let i = numero-1; i >0; i--){
+    resul *= i;   
 }
 console.log(resul);
+
+
+
