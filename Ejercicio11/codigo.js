4f9fb432e4c7ca56e7db4f2ec6691666e0d7f536
guntherscherzer <guntherscherzer97@gmail.com>
2023-09-05 17:58:30 -0300

    agregado de Casos de prueba

--- Ejercicio11/codigo.js
+++ Ejercicio11/codigo.js
@@ -38,34 +38,62 @@ asignar(){};
 const estudiante1 = new Estudiante("Juan",20,"Masculino","Matematicas","nivel1");
 const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
  
-//Casos de prueba
+// Casos de prueba
 
 // 1) Poder instanciar las diferentes clases sin obtener error alguno.
-
-function test1(){
-
-}
-
-// 2) Instanciar las clases sin pasarle parametros de construccion y que tire error.
-
-function test2(){
+function test1() {
+    console.log("test1:");
+    console.log(estudiante1);
+    console.log(profesor1);
+  }
+  
+  // 2) Instanciar las clases sin pasarles parámetros de construcción y que tire error.
+  function test2() {
+    console.log("test2:");
+    const estudiante2 = new Estudiante();
+    const profesor2 = new Profesor();
+    console.log(estudiante2);
+    console.log(profesor2);
+    if (!estudiante2.nombre || !estudiante2.edad || !estudiante2.genero) {
+        console.log("Error al crear una instancia de Estudiante.");
+    }
     
-}
-
-// 3) Instanciar la clase Persona y poder invocar el metodo obtDetalles, mostrando en pantalla los datos de la persona.
-
-function test3(){
+    if (!profesor2.nombre || !profesor2.edad || !profesor2.genero) {
+        console.log("Error al crear una instancia de Profesor.");
+    }
     
+    console.log("No se produce ningun error.");
 }
 
-// 4) Instanciar las clases de Estudiante y Profesor y corroborar que contenga las propiedades heredadas.
-
-function test4(){
-    
-}
-
-// 5) Instanciar las clases de Estudiante y Profesor y corroborar que contenga el metodo heredado.
-
-function test5(){
-    
-}
+  
+  // 3) Instanciar la clase Persona y poder invocar el método obtDetalles, mostrando en pantalla los datos de la persona.
+  function test3() {
+    console.log("test3:");
+    const persona = new Persona("Ana", 30, "Femenino");
+    persona.obtDetalles();
+  }
+  
+  // 4) Instanciar las clases de Estudiante y Profesor y corroborar que contengan las propiedades heredadas.
+  function test4() {
+    console.log("test4:");
+    console.log("Estudiante:");
+    console.log(estudiante1);
+    console.log("Profesor:");
+    console.log(profesor1);
+  }
+  
+  // 5) Instanciar las clases de Estudiante y Profesor y corroborar que contengan el método heredado.
+  function test5() {
+    console.log("test5:");
+    console.log("Estudiante:");
+    estudiante1.obtDetalles();
+    console.log("Profesor:");
+    profesor1.obtDetalles();
+  }
+  
+  // Ejecutar los casos de prueba
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
