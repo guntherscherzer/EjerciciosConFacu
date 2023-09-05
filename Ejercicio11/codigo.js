a3f8ce4e73eb72e3b6d7359ddee217983bb26407
Facundo Sautu <fsautu@tecnoaccion.com.ar>
2023-09-05 12:19:06 -0300

    Agregado de los casos de uso para el Ejercicio 11

--- Ejercicio11/codigo.js
+++ Ejercicio11/codigo.js
@@ -15,27 +15,37 @@ class Persona {
         console.log(this.nombre,this.edad,this.genero);
     }
 }
- //Objeto Estudiante
- class Estudiante extends Persona{
-    constructor(nombre,edad,genero,curso,grupo){
-        super(nombre,edad,genero);
-        this.curso = curso;
-        this.grupo = grupo;
-    }
-    registrar() {}
- }
- //objeto Profesor
- class Profesor extends Persona{
-    constructor (nombre,edad,genero,asignatura,nivel){
-        super(nombre,edad,genero);
-        this.asignatura = asignatura
-        this.nivel = nivel;
-    }
-    asignar(){};
- }
+//Objeto Estudiante
+class Estudiante extends Persona{
+constructor(nombre,edad,genero,curso,grupo){
+    super(nombre,edad,genero);
+    this.curso = curso;
+    this.grupo = grupo;
+}
+registrar() {}
+}
+//objeto Profesor
+class Profesor extends Persona{
+constructor (nombre,edad,genero,asignatura,nivel){
+    super(nombre,edad,genero);
+    this.asignatura = asignatura
+    this.nivel = nivel;
+}
+asignar(){};
+}
 
- //Casos de prueba
- const estudiante1 = new Estudiante("Juan",20,"Masculino","Matematicas","nivel1");
- const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
-
+// Instancia de Objetos
+const estudiante1 = new Estudiante("Juan",20,"Masculino","Matematicas","nivel1");
+const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
  
+//Casos de prueba
+
+// 1) Poder instanciar las diferentes clases sin obtener error alguno.
+
+// 2) Instanciar las clases sin pasarle parametros de construccion y que tire error.
+
+// 3) Instanciar la clase Persona y poder invocar el metodo obtDetalles, mostrando en pantalla los datos de la persona.
+
+// 4) Instanciar las clases de Estudiante y Profesor y corroborar que contenga las propiedades heredadas.
+
+// 5) Instanciar las clases de Estudiante y Profesor y corroborar que contenga el metodo heredado.
