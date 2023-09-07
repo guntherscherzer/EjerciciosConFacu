4c3234d2c7bedb05f4a8601a8b67d5ec959ed8fc
guntherscherzer <guntherscherzer97@gmail.com>
2023-09-07 18:23:50 -0300

    correccion de identacion ejercicio 11

--- Ejercicio11/codigo.js
+++ Ejercicio11/codigo.js
@@ -17,20 +17,20 @@ class Persona {
 }
 //Objeto Estudiante
 class Estudiante extends Persona{
-constructor(nombre,edad,genero,curso,grupo){
-    super(nombre,edad,genero);
-    this.curso = curso;
-    this.grupo = grupo;
-}
+  constructor(nombre,edad,genero,curso,grupo){
+      super(nombre,edad,genero);
+      this.curso = curso;
+      this.grupo = grupo;
+  }
 registrar() {}
 }
 //objeto Profesor
 class Profesor extends Persona{
-constructor (nombre,edad,genero,asignatura,nivel){
-    super(nombre,edad,genero);
-    this.asignatura = asignatura
-    this.nivel = nivel;
-}
+  constructor (nombre,edad,genero,asignatura,nivel){
+      super(nombre,edad,genero);
+      this.asignatura = asignatura
+      this.nivel = nivel;
+  }
 asignar(){};
 }
 
@@ -45,51 +45,51 @@ function test1() {
   const estudiante1 = new Estudiante("Juan",20,"Masculino","Matematicas","nivel1");
   const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
   return("test1:",estudiante1,profesor1);
-  }
+}
   
-  // 2) Instanciar las clases sin pasarles parámetros de construcción y que tire error.
-  function test2() {
-    const estudiante2 = new Estudiante();
-    const profesor2 = new Profesor();
-    if (!estudiante2.nombre || !estudiante2.edad || !estudiante2.genero || !profesor2.nombre || !profesor2.edad || !profesor2.genero) {
-        return(true);
-      }return(false)
-    }
+// 2) Instanciar las clases sin pasarles parámetros de construcción y que tire error.
+function test2() {
+  const estudiante2 = new Estudiante();
+  const profesor2 = new Profesor();
+  if (!estudiante2.nombre || !estudiante2.edad || !estudiante2.genero || !profesor2.nombre || !profesor2.edad || !profesor2.genero) {
+      return(true);
+    }return(false)
+}
     
     
 
-  
-  // 3) Instanciar la clase Persona y poder invocar el método obtDetalles, mostrando en pantalla los datos de la persona.
-  function test3() {
-    const persona = new Persona("Ana", 30, "Femenino");
-    const existeElmetodo = "obtDetalles" in persona;
-    persona.obtDetalles();
-    return(existeElmetodo);
-  }
-  
-  // 4) Instanciar las clases de Estudiante y Profesor y corroborar que contengan las propiedades heredadas.
-  function test4() {
-    const estudiante1 = new Estudiante(20,"Masculino","Matematicas","nivel1");
-    const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
-    if (estudiante1.hasOwnProperty("nombre") == true && estudiante1.hasOwnProperty("edad") == true && estudiante1.hasOwnProperty("genero") == true 
-      && profesor1.hasOwnProperty("nombre") == true && profesor1.hasOwnProperty("edad") == true && profesor1.hasOwnProperty("genero") == true ) {
-      return(true);
-      
-    }return(false);
-  }
-  
-  // 5) Instanciar las clases de Estudiante y Profesor y corroborar que contengan el método heredado.
-  function test5() {
-    const estudiante1 = new Estudiante(20,"Masculino","Matematicas","nivel1");
-    const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
-    const estudianteTieneMetodo = "obtDetalles" in estudiante1;
-    const profesorTieneMetodo = "obtDetalles" in profesor1;
-    return("Estudiante:",estudianteTieneMetodo,"Profesor:",profesorTieneMetodo);
-  }
-  
-  // Ejecutar los casos de prueba
-  test1();
-  test2();
-  test3();
-  test4();
-  test5();
+
+// 3) Instanciar la clase Persona y poder invocar el método obtDetalles, mostrando en pantalla los datos de la persona.
+function test3() {
+  const persona = new Persona("Ana", 30, "Femenino");
+  const existeElmetodo = "obtDetalles" in persona;
+  persona.obtDetalles();
+  return(existeElmetodo);
+}
+
+// 4) Instanciar las clases de Estudiante y Profesor y corroborar que contengan las propiedades heredadas.
+function test4() {
+  const estudiante1 = new Estudiante(20,"Masculino","Matematicas","nivel1");
+  const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
+  if (estudiante1.hasOwnProperty("nombre") == true && estudiante1.hasOwnProperty("edad") == true && estudiante1.hasOwnProperty("genero") == true 
+    && profesor1.hasOwnProperty("nombre") == true && profesor1.hasOwnProperty("edad") == true && profesor1.hasOwnProperty("genero") == true ) {
+    return(true);
+    
+  }return(false);
+}
+
+// 5) Instanciar las clases de Estudiante y Profesor y corroborar que contengan el método heredado.
+function test5() {
+  const estudiante1 = new Estudiante(20,"Masculino","Matematicas","nivel1");
+  const profesor1 = new Profesor("Wilfred",28,"Transexual","Historia","Nivel2");
+  const estudianteTieneMetodo = "obtDetalles" in estudiante1;
+  const profesorTieneMetodo = "obtDetalles" in profesor1;
+  return("Estudiante:",estudianteTieneMetodo,"Profesor:",profesorTieneMetodo);
+}
+
+// Ejecutar los casos de prueba
+test1();
+test2();
+test3();
+test4();
+test5();
--- Ejercicio12/codigo.js
+++ Ejercicio12/codigo.js
@@ -2,37 +2,6 @@
 // Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados.
 // Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 36.000 veces esta operación.
 
-// Crear un objeto para almacenar las frecuencias de las sumas
-const sumasFrecuencia = {};
-
-// Función para simular el lanzamiento de un dado
-function lanzarDado() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-// Realizar 36,000 lanzamientos de dados
-const numLanzamientos = 36000;
-let lanzamientos = 0;
-
-while (lanzamientos < numLanzamientos) {
-  const dado1 = lanzarDado();
-  const dado2 = lanzarDado();
-  const suma = dado1 + dado2;
-  
-  if (sumasFrecuencia[suma]) {
-    sumasFrecuencia[suma]++;
-  } else {
-    sumasFrecuencia[suma] = 1;
-  }
-
-  lanzamientos++;
-}
-
-// Mostrar las frecuencias de las sumas
-for (const suma in sumasFrecuencia) {
-  console.log(`Suma ${suma}: ${sumasFrecuencia[suma]} veces`);
-}
-
 
 
 
